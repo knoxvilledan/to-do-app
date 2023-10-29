be03cee4f02d334bcc9bfd0e8e3331550bb98990
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2023-10-29 18:50:17 -0400

    Day one. New Journey.Initial commit with .gitignore

---
+++ main.js
@@ -0,0 +1,30 @@
+// Get the button element. MOVED all of this to scripts.js in public folder
+/* const btn = document.getElementById('colorChangeBtn');
+
+// Add a click event listener to the button
+btn.addEventListener('click', function() {
+    // Generate a random color
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    
+    // Change the background color of the body to the random color
+    document.body.style.backgroundColor = randomColor;
+});
+*/
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));;
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+
+  
